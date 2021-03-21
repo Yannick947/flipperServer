18b702e3055b215d1e44f644e1980c05f41540c7
Yannick <yannick.schrempp@web.de>
2021-03-21 20:10:10 +0100

    Refactor balls_fallen to vector

--- src/entities/game/game_result.cpp
+++ src/entities/game/game_result.cpp
@@ -1,10 +1,10 @@
-#include <list>
 #include "entities/game/game_result.h"
+#include <vector>
 
 using namespace std; 
 
 
-GameResult::GameResult(list<int> player_balls, int looser_player_id){
+GameResult::GameResult(vector<int> player_balls, int looser_player_id){
     m_looser_player_id = looser_player_id; 
     m_player_balls = player_balls; 
 }
@@ -12,10 +12,8 @@ GameResult::GameResult(list<int> player_balls, int looser_player_id){
 void GameResult::print(){
  //  cout << "Looser is player with id ";
  //  cout << to_string(this->m_looser_player_id) << endl;
-    auto player_ball_iterator = this->m_player_balls.begin();
 
     for (int i = 0; i < this->m_player_balls.size(); i++){
-        //cout << "Player with id " << to_string(i) << " lost " << to_string(*player_ball_iterator) << " balls" << endl;
-        advance(player_ball_iterator, 1);
+        //cout << "Player with id " << to_string(i) << " lost " << to_string(this->m_player_balls[i]) << " balls" << endl;
     }
 }
--- src/entities/game/game_result.h
+++ src/entities/game/game_result.h
@@ -1,7 +1,7 @@
 #ifndef GAMERESULT_H
 #define GAMERESULT_H
 
-#include <list>
+#include <vector>
 
 using namespace std; 
 
@@ -9,10 +9,10 @@ using namespace std;
 class GameResult {
     private: 
         int m_looser_player_id; 
-        list<int> m_player_balls; 
+        vector<int> m_player_balls; 
 
     public: 
-        GameResult(list<int> player_balls, int looser_player_id);
+        GameResult(vector<int> player_balls, int looser_player_id);
 
         int getWinner();
         int getBallsByPlayer(int player_id);
